5b87dd112306aa8c824190717081ccce377a30d3
Exequiel <aaronleal03@gmail.com>
2024-06-03 18:36:31 -0400

    Serializers

--- ClinicaBackend/myapp/serializers.py
+++ ClinicaBackend/myapp/serializers.py
@@ -1,22 +1,28 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User 
 from .models import Contacto,Medico,Paciente,Horas
 
 class ContactoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contacto
-        fields ="all"
+        fields ="__all__"
 
 class MedicoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medico
-        fields ="all"
+        fields ="__all__"
 
 class PacienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Paciente
-        fields ="all"
-    
+        fields ="__all__"
+
 class HorassSerializer(serializers.ModelSerializer):
     class Meta:
         model = Horas
-        fields ="_all"
+        fields ="__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
