db71b78020f51f9d16d47ff27318c290a33fa7c0
Exequiel <aaronleal03@gmail.com>
2024-06-03 18:18:49 -0400

    Clases

--- ClinicaBackend/myapp/models.py
+++ ClinicaBackend/myapp/models.py
@@ -14,14 +14,34 @@ class Contacto(models.Model):
 
 class Medico(models.Model):
     nombrem = models.CharField(max_length=100)
-    rut = models.CharField(max_length=9)
+    rut = models.CharField(max_length=11, primary_key=True, unique=True)
     especialidad = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.nombrem} - {self.especialidad}"
+        return f" {self.rut} - {self.nombrem} - {self.especialidad}"
     
 class Paciente(models.Model):
     nombreCompleto = models.CharField(max_length=80)
-    rut= models.CharField(max_length=11)
+    rut= models.CharField(max_length=11, primary_key=True, unique=True )
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    especialidad = models.CharField(max_length=100)
+    prevision = models.CharField(max_length=60)
+
+    def __str__(self):
+        return f"{self.nombreCompleto} - {self.rut} - {self.especialidad} - {self.medico} - {self.prevision} "
+class Horas(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    prevision_paciente = models.CharField(max_length=60)
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    especialidad = models.CharField(max_length=50)
+    fecha = models.DateField()
+    hora = models.TimeField() 
+
+    def __str__(self):
+        return f"{self.paciente} - {self.prevision_paciente} - {self.especialidad} - {self.medico} - {self.fecha} - {self.hora} "
+    
+
+
+
 
     
