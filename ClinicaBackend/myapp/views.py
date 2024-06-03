9037976ae1a95b49e3b275a5a871c3be921d8606
Estebandido123 <este.toledo@duocuc.cl>
2024-06-03 18:46:12 -0400

    Vistas

--- ClinicaBackend/myapp/views.py
+++ ClinicaBackend/myapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Contacto,Medico,Paciente,Horas
+from .serializers import ContactoSerializer, MedicoSerializer,PacienteSerializer,HorassSerializer
 
 # Create your views here.
+class ContactoViewSet(viewsets.ModelViewSet):
+    queryset = Contacto.objects.all()
+    serializer_class = ContactoSerializer
+
+class MedicoViewSet(viewsets.ModelViewSet):
+    queryset = Medico.objects.all()
+    serializer_class = MedicoSerializer
+
+class PacienteViewSet(viewsets.ModelViewSet):
+    queryset = Medico.objects.all()
+    serializer_class = PacienteSerializer
+
+class HorasViewSet(viewsets.ModelViewSet):
+    queryset = Paciente.objects.all()
+    serializer_class = PacienteSerializer
